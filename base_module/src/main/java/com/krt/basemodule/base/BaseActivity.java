81e1fbbbe71885aedb19c0ea0e6b5342fd969598
krt <you@gmial.com>
2018-11-20 10:27:31 +0800

    Add base module(base code)

---
+++ base_module/src/main/java/com/krt/basemodule/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.krt.basemodule.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author KRT
+ * 2018/11/20
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ base_module/src/main/java/com/krt/basemodule/utils/ToastUtils.java
@@ -0,0 +1,36 @@
+package com.krt.basemodule.utils;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * @author KRT
+ * 2018/11/20
+ */
+public class ToastUtils {
+    private static Toast mToast;
+
+    private ToastUtils() {
+        throw new AssertionError();
+    }
+
+    @SuppressLint("ShowToast")
+    public static void init(Context context){
+        mToast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
+    }
+
+    public static void show(int resId){
+        if(null != mToast){
+            mToast.setText(resId);
+            mToast.show();
+        }
+    }
+
+    public static void show(CharSequence msg){
+        if(null != mToast && null != msg && msg.length() > 0){
+            mToast.setText(msg);
+            mToast.show();
+        }
+    }
+}
