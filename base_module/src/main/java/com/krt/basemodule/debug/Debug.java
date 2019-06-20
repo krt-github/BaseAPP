6c9b628adedac40210d55501faf288abc5f3a37b
krt <krt@gmail.com>
2019-06-20 13:49:30 +0800

    Modify parameter String to Object

--- base_module/src/main/java/com/krt/basemodule/debug/Debug.java
+++ base_module/src/main/java/com/krt/basemodule/debug/Debug.java
@@ -2,12 +2,14 @@ package com.krt.basemodule.debug;
 
 import android.util.Log;
 
+import com.krt.base.BuildConfig;
+
 /**
  * @author KRT
  * 2018/11/20
  */
 public class Debug {
-    private static final boolean ENABLE_DEBUG = true;
+    private static final boolean ENABLE_DEBUG = BuildConfig.DEBUG;
 
     private Debug() {}
 
@@ -15,26 +17,26 @@ public class Debug {
         return ENABLE_DEBUG;
     }
 
-    public static void d(String tag, String msg) {
+    public static void d(String tag, Object msg) {
         if (isEnableDebug()) {
             Log.d(tag, checkNull(msg));
         }
     }
 
-    public static void i(String tag, String msg) {
+    public static void i(String tag, Object msg) {
         if (isEnableDebug()) {
             Log.i(tag, checkNull(msg));
         }
     }
 
-    public static void e(String tag, String msg) {
+    public static void e(String tag, Object msg) {
         if (isEnableDebug()) {
             Log.e(tag, checkNull(msg));
         }
     }
 
-    private static String checkNull(String msg) {
-        return null == msg ? "[NULL]" : msg;
+    private static String checkNull(Object msg) {
+        return null == msg ? "[NULL]" : msg.toString();
     }
 
 }
